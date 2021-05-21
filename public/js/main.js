7fb1a727543d99270f124c329ab4ae472f3b64c3
Levyks <marcio.levy20@gmail.com>
2021-05-21 16:28:12 -0300

    Started the implementation of route manager

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,12 @@
 $(function(){
 
+    function SVGtoCommonObj(pt){
+        return {
+            x: pt.x,
+            y: pt.y
+        }
+    }
+
     function drawLine(p1,p2){
         ctx.lineWidth = 1;
         ctx.beginPath();
@@ -58,36 +65,38 @@ $(function(){
     }
 
     function closeLoop(){
-        drawLine(waypoints[waypoints.length-1],waypoints[0]);
-        drawTriangleInTheMiddle(waypoints[waypoints.length-1],waypoints[0]);
+        drawLine(route[currentRouteStep].waypoints[route[currentRouteStep].waypoints.length-1],route[currentRouteStep].waypoints[0]);
+        drawTriangleInTheMiddle(route[currentRouteStep].waypoints[route[currentRouteStep].waypoints.length-1],route[currentRouteStep].waypoints[0]);
     }
     
-    function addWaypoint(pt, drawLine = true){
+    function addWaypoint(pt){
         pt=SVGtoCommonObj(pt);
-        lastWaypoint=pt; 
 
-        waypoints.push(pt);
+        route[currentRouteStep].waypoints.push(pt);
 
-        $("#waypoints-display").text(`Quantity: ${waypoints.length}`);
+        $("#route[currentRouteStep].waypoints-display").text(`Quantity: ${route[currentRouteStep].waypoints.length}`);
 
         redraw();
     }
 
     function drawAllWPs(){
         let count = 0
-        let waypointsToIterate = waypoints.slice();
 
-        waypointsToIterate.forEach(wp => {
-            const pt = {
-                x: wp.x,
-                y: wp.y
-            }
+        if(currentRouteStep){
+            const prevStepWPS = route[currentRouteStep-1].waypoints
+            const lastWaypointFromPrevStep = prevStepWPS[prevStepWPS.length-1];
+            drawWP(lastWaypointFromPrevStep, true, "orange");
+        }
+
+
+        route[currentRouteStep].waypoints.forEach(wp => {
+            const pt = SVGtoCommonObj(wp);
             let color;
             switch (count){
                 case 0:
                     color = "green";
                     break; 
-                case waypoints.length-1:
+                case route[currentRouteStep].waypoints.length-1:
                     color = "blue";
                     break;
                 default:
@@ -95,10 +104,10 @@ $(function(){
                     break;
             }
                     
-            drawWP(pt, !count, color);
+            drawWP(pt, !(count || currentRouteStep), color);
             count+=1;
         });
-        if(loopRoute && waypoints.length>2) closeLoop();
+        if(route[currentRouteStep].loop && route[currentRouteStep].waypoints.length>2) closeLoop();
     }
 
     const defaultWaypointColor = "red";
@@ -111,8 +120,6 @@ $(function(){
     const canvas = $("#mainCanvas");
     const canvasDOM = canvas[0]
     const ctx = canvas[0].getContext("2d");
-
-    let waypoints = [];
 
     let dragged = false;
 
@@ -127,9 +134,8 @@ $(function(){
         allowMove = e.target.checked;
     })
 
-    let loopRoute=false;
     $("#loop-cb").change((e)=>{
-        loopRoute = e.target.checked;
+        route[currentRouteStep].loop = e.target.checked;
         redraw();
     })
 
@@ -179,56 +185,37 @@ $(function(){
         
     })
 
-    $("#calibrate-btn").click(calibrate);
-
-
-    let calibration = {}
-    function calibrate(){
-        const diffXinMap = markers.a.coords.x - markers.b.coords.x;
-        const diffXinGame = markers.a.inGameCoords.x - markers.b.inGameCoords.x;
-        calibration.ratioX = diffXinGame/diffXinMap;
-
-        const diffYinMap = markers.a.coords.y - markers.b.coords.y;
-        const diffYinGame = markers.a.inGameCoords.y - markers.b.inGameCoords.y;
-        calibration.ratioY = diffYinGame/diffYinMap;
-
-        calibration.constX = markers.a.inGameCoords.x - (markers.a.coords.x*calibration.ratioX);
-
-        calibration.constY = markers.a.inGameCoords.y - (markers.a.coords.y*calibration.ratioY);
-
-        console.log(calibration);
-        
-    }
+    
 
     $("#remove-all-btn").click(()=>{
-        if(waypoints.length==0){
+        if(route[currentRouteStep].waypoints.length==0){
             alert("There is no waypoint to remove");
             return;
         }
         if(confirm("Remove all waypoitns?")){
-            waypoints.length=0;
-            $("#waypoints-display").text(`Quantity: ${waypoints.length}`);
+            route[currentRouteStep].waypoints.length=0;
+            $("#route[currentRouteStep].waypoints-display").text(`Quantity: ${route[currentRouteStep].waypoints.length}`);
         }
         redraw();
     });
 
     $("#remove-first-btn").click(()=>{
-        if(waypoints.length==0){
+        if(route[currentRouteStep].waypoints.length==0){
             alert("There is no waypoint to remove");
             return;
         }
-        waypoints.shift();
-        $("#waypoints-display").text(`Quantity: ${waypoints.length}`);
+        route[currentRouteStep].waypoints.shift();
+        $("#route[currentRouteStep].waypoints-display").text(`Quantity: ${route[currentRouteStep].waypoints.length}`);
         redraw();
     });
 
     $("#remove-last-btn").click(()=>{
-        if(waypoints.length==0){
+        if(route[currentRouteStep].waypoints.length==0){
             alert("There is no waypoint to remove");
             return;
         }
-        waypoints.pop();
-        $("#waypoints-display").text(`Quantity: ${waypoints.length}`);
+        route[currentRouteStep].waypoints.pop();
+        $("#route[currentRouteStep].waypoints-display").text(`Quantity: ${route[currentRouteStep].waypoints.length}`);
         redraw();
     });
 
@@ -241,23 +228,23 @@ $(function(){
     let isPlacementReverted = false;
     $("#revert-btn").click(()=>{
         isPlacementReverted = !isPlacementReverted;
-        waypoints.reverse();
-        lastWaypoint = waypoints[waypoints.length-1];
+        route[currentRouteStep].waypoints.reverse();
+        lastWaypoint = route[currentRouteStep].waypoints[route[currentRouteStep].waypoints.length-1];
         redraw();
     });
 
     function getExportableRoute(relativeTo = "game"){
         const output = {
             "relativeTo": relativeTo,
-            "loop": loopRoute,
-            "waypoints":[]
+            "loop": route[currentRouteStep].loop,
+            "route[currentRouteStep].waypoints":[]
         };
         if(relativeTo == "game"){
-            waypoints.forEach((wp) => {
-                output.waypoints.push(translateToGame(wp));
+            route[currentRouteStep].waypoints.forEach((wp) => {
+                output.route[currentRouteStep].waypoints.push(translateToGame(wp));
             });
         }else{
-            output.waypoints = waypoints;
+            output.route[currentRouteStep].waypoints = route[currentRouteStep].waypoints;
         }
         return output;
     }
@@ -281,7 +268,7 @@ $(function(){
         const form = e.target;
         const relativeTo = form.elements['export-relative-to'].value;
 
-        if(!waypoints.length){
+        if(!route[currentRouteStep].waypoints.length){
             alert("There is no waypoint to export");
             return;
         }
@@ -321,7 +308,7 @@ $(function(){
                 console.log("Legacy JSON detected");
                 dataToImport.loop = false;
                 dataToImport.relativeTo = "map";
-                dataToImport.waypoints = dataParsed;
+                dataToImport.route[currentRouteStep].waypoints = dataParsed;
             }else{
                 dataToImport = dataParsed;
             }
@@ -336,17 +323,17 @@ $(function(){
                 return;
             }
 
-            waypoints.length = 0;
+            route[currentRouteStep].waypoints.length = 0;
 
             if(relativeTo == "game"){
-                dataToImport.waypoints.forEach((wp) => {
-                    waypoints.push(translateToMap(wp));        
+                dataToImport.route[currentRouteStep].waypoints.forEach((wp) => {
+                    route[currentRouteStep].waypoints.push(translateToMap(wp));        
                 });
             }else{
-                waypoints = dataToImport.waypoints;
+                route[currentRouteStep].waypoints = dataToImport.route[currentRouteStep].waypoints;
             }
             
-            $("#waypoints-display").text(`Quantity: ${waypoints.length}`);  
+            $("#route[currentRouteStep].waypoints-display").text(`Quantity: ${route[currentRouteStep].waypoints.length}`);  
             $("#modal-out-import").css("display","none");
             form.reset();  
             redraw();
@@ -368,38 +355,6 @@ $(function(){
         }
     }
 
-    $("#calib-json-download-btn").click(()=>{
-        if(jQuery.isEmptyObject(calibration)){
-            alert("No calibration was done");
-            return;
-        }
-        const dataToExport = {
-            calibration: calibration,
-            markers: markers
-        }
-        var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(dataToExport, null, 2));
-        var dlAnchorElem = document.getElementById('calib-json-download-a');
-        dlAnchorElem.setAttribute("href",     dataStr     );
-        dlAnchorElem.setAttribute("download", "calibExport.json");
-        dlAnchorElem.click();
-    })
-
-    $("#calib-json-input").change((e)=>{
-        let reader = new FileReader();
-        reader.onload = (data) => {
-            const importedData = JSON.parse(data.target.result);        
-            markers = importedData.markers;
-            calibration = importedData.calibration
-            $("#marker-a-x").val(markers.a.inGameCoords.x); 
-            $("#marker-a-y").val(markers.a.inGameCoords.y);
-            $("#marker-b-x").val(markers.b.inGameCoords.x);
-            $("#marker-b-y").val(markers.b.inGameCoords.y);
-            calibrate(); 
-            redraw();
-        }
-        reader.readAsText(e.target.files[0]);
-    })
-
     const defaultDistanceBwWp = 10
     let distanceBetweenWPs = defaultDistanceBwWp;
     $("#wp-distance-input").change((e) => {
@@ -416,14 +371,19 @@ $(function(){
 
     let lastWaypoint = {};
 
+    /*-------------------------------------------------
+     * MOUSE EVENTS HANDLERS */
+
     let dragStart;
+    let lastX;
+    let lastY;
 
     canvas.on("mousedown", (evt) => {
         document.body.style.mozUserSelect = document.body.style.webkitUserSelect = document.body.style.userSelect = 'none';
         lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
         lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
         dragStart = ctx.transformedPoint(lastX,lastY);
-        if(!allowMove && !markers.toPlace) addWaypoint(dragStart,waypoints.length);
+        if(!allowMove && !markers.toPlace) addWaypoint(dragStart,route[currentRouteStep].waypoints.length);
         dragged = false;
     });
 
@@ -461,11 +421,29 @@ $(function(){
         } 
     });
 
-    function SVGtoCommonObj(pt){
-        return {
-            x: pt.x,
-            y: pt.y
-        }
+    //-------------------------------------------------
+
+    /*-------------------------------------------------
+     * CALIBRATION AND MARKERS */
+
+    $("#calibrate-btn").click(calibrate);
+
+    let calibration = {}
+    function calibrate(){
+        const diffXinMap = markers.a.coords.x - markers.b.coords.x;
+        const diffXinGame = markers.a.inGameCoords.x - markers.b.inGameCoords.x;
+        calibration.ratioX = diffXinGame/diffXinMap;
+
+        const diffYinMap = markers.a.coords.y - markers.b.coords.y;
+        const diffYinGame = markers.a.inGameCoords.y - markers.b.inGameCoords.y;
+        calibration.ratioY = diffYinGame/diffYinMap;
+
+        calibration.constX = markers.a.inGameCoords.x - (markers.a.coords.x*calibration.ratioX);
+
+        calibration.constY = markers.a.inGameCoords.y - (markers.a.coords.y*calibration.ratioY);
+
+        console.log(calibration);
+        
     }
 
     function placeMarker(marker, pt){
@@ -498,6 +476,44 @@ $(function(){
         if(markers.a.placed) drawMarker(markers.a);
         if(markers.b.placed) drawMarker(markers.b);
     }
+
+    $("#calib-json-download-btn").click(()=>{
+        if(jQuery.isEmptyObject(calibration)){
+            alert("No calibration was done");
+            return;
+        }
+        const dataToExport = {
+            calibration: calibration,
+            markers: markers
+        }
+        var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(dataToExport, null, 2));
+        var dlAnchorElem = document.getElementById('calib-json-download-a');
+        dlAnchorElem.setAttribute("href",     dataStr     );
+        dlAnchorElem.setAttribute("download", "calibExport.json");
+        dlAnchorElem.click();
+    });
+
+    $("#calib-json-input").change((e)=>{
+        let reader = new FileReader();
+        reader.onload = (data) => {
+            const importedData = JSON.parse(data.target.result);        
+            markers = importedData.markers;
+            calibration = importedData.calibration
+            $("#marker-a-x").val(markers.a.inGameCoords.x); 
+            $("#marker-a-y").val(markers.a.inGameCoords.y);
+            $("#marker-b-x").val(markers.b.inGameCoords.x);
+            $("#marker-b-y").val(markers.b.inGameCoords.y);
+            calibrate(); 
+            redraw();
+        }
+        reader.readAsText(e.target.files[0]);
+    });
+
+    //-------------------------------------------------
+
+
+    /*-------------------------------------------------
+     * ZOOM */
 
     const minZoom = -12;
     const maxZoom = 24;
@@ -538,23 +554,90 @@ $(function(){
     canvasDOM.addEventListener('DOMMouseScroll',handleScroll,false);
     canvasDOM.addEventListener('mousewheel',handleScroll,false);
 
-    function checkLimits(pt){
-        const ptTL = ctx.reverseTransformedPoint(0,0);
-        const ptBR = ctx.reverseTransformedPoint(image.width,image.height);
-        const translateX = pt.x-dragStart.x;
-        const translateY = pt.y-dragStart.y;
+    //-------------------------------------------------
 
-        if(translateX > 0 && ptTL.x+translateX > 0) return false;
-        if(translateY > 0 && ptTL.y+translateY > 0) return false; 
+    /*-------------------------------------------------
+     * MODALS */
 
-        if(translateX < 0 && ptBR.x+translateX < canvasDOM.width) return false;
-        if(translateY < 0 && ptBR.y+translateY < canvasDOM.height) return false; 
+    $("#open-manage-modal").click(updateManageTable);
 
-        return true;
+    $(".open-modal-btn").click((e) => {
+        $("#"+(e.target.getAttribute("modal"))).css("display","block");
+    })
+
+    $(".close-modal").click((e) => {
+        $(e.target).closest($(".modal-outside")).css("display","none");
+    })
+
+    $(window).click((e) => {
+        const target = $(e.target)
+        if(target.hasClass("modal-outside")){
+            target.css("display","none");
+        }
+    });
+
+    //-------------------------------------------------
+
+    /*-------------------------------------------------
+     * ROUTE MANAGING */
+
+    let currentRouteStep = 0;
+    let route = [
+        {
+            loop: false,
+            waypoints: []
+        },
+    ];
+    
+    function updateManageTable(){
+        const tableBody = $("#route-table tbody");
+        tableBody.empty();
+        let count = 0;
+        route.forEach((routeStep) => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+            <th scope="row">${count+1}</th>
+            <td>                                             
+                <button class="btn route-table-btn"><i class="fa fa-arrow-up"></i></button>
+                <button class="btn route-table-btn"><i class="fa fa-arrow-down"></i></button> 
+                <button class="btn route-table-btn"><i class="fa fa-times text-danger"></i> </i></button>  
+            </td>
+            <td>${routeStep.waypoints.length}</td>
+            <td>
+                <input class="form-check-input" type="radio" name="current-route" value="${count}" ${currentRouteStep == count ? "checked" : ""}>
+            </td>
+            <td>
+                <div class="input-group input-group-sm">
+                    <input type="number" class="form-control input-sm loop-quantity-input" ${routeStep.loop ? "" : "disabled"}>
+                </div>     
+            </td>
+            `
+            tableBody[0].appendChild(row);
+            
+            count++;
+        });
+        const radioInputs = $('input[type=radio][name=current-route]');
+        radioInputs.off("change");
+        radioInputs.on("change",function() {
+            currentRouteStep = parseInt(($(this).val()));
+            redraw();
+        });
+        
     }
 
+    
 
-    trackTransforms(ctx);  
+    $("#new-step-btn").click((e) => {
+        route.push({
+            loop: false,
+            waypoints: []
+        });
+        currentRouteStep = route.length-1;
+        updateManageTable();
+    });
+
+    //-------------------------------------------------
+
     function redraw(){
         var p1 = ctx.transformedPoint(0,0);
         var p2 = ctx.transformedPoint(canvasDOM.width,canvasDOM.height);
@@ -565,7 +648,7 @@ $(function(){
         drawAllWPs();
         drawAllMarkers();
     }
-
+ 
     function trackTransforms(ctx){
 		var svg = document.createElementNS("http://www.w3.org/2000/svg",'svg');
 		var xform = svg.createSVGMatrix();
@@ -626,6 +709,7 @@ $(function(){
 			return pt.matrixTransform(xform);
 		}
 	}
+    trackTransforms(ctx); 
     
     function resizeCanvas(){
         canvasDOM.width = cvWrapper.width();
@@ -637,23 +721,5 @@ $(function(){
     $(window).resize(resizeCanvas);
     
     resizeCanvas();
-
-
-
-    $(".open-modal-btn").click((e) => {
-        $("#"+(e.target.getAttribute("modal"))).css("display","block");
-    })
-
-    $(".close-modal").click((e) => {
-        $(e.target).closest($(".modal-outside")).css("display","none");
-    })
-
-    // When the user clicks anywhere outside of the modal, close it
-    $(window).click((e) => {
-        const target = $(e.target)
-        if(target.hasClass("modal-outside")){
-            target.css("display","none");
-        }
-    });
 
 });
